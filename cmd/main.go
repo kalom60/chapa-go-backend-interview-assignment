e11e2096ad0938e09e896648de67f1d53a17888a
kalom60 <39136564+kalom60@users.noreply.github.com>
2025-07-09 02:55:04 +0300

    main

--- cmd/main.go
+++ cmd/main.go
@@ -1,19 +1,57 @@
 package main
 
 import (
-	"fmt"
+	"context"
 	"log"
+	"net/http"
 	"os"
+	"os/signal"
+	"syscall"
+	"time"
 
 	"github.com/kalom60/chapa-go-backend-interview-assignment/config"
+	"github.com/kalom60/chapa-go-backend-interview-assignment/internal/bank"
+	"github.com/kalom60/chapa-go-backend-interview-assignment/internal/repository"
+	"github.com/kalom60/chapa-go-backend-interview-assignment/internal/server"
 )
 
 func main() {
 	cfg, err := config.NewConfig()
 	if err != nil {
 		log.Fatal(err.Error())
-		os.Exit(1)
 	}
 
-	fmt.Println("Here", cfg)
+	db, closeDB, err := repository.OpenDB(cfg.DbUrl)
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+	defer closeDB()
+
+	store := repository.NewStore(db)
+	bank := bank.New(store)
+
+	srv := server.NewServer(cfg.Port, bank)
+
+	shutdownError := make(chan error)
+
+	go func() {
+		quit := make(chan os.Signal, 1)
+		signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
+		<-quit
+
+		log.Println("shutting down server...")
+		ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+		defer cancel()
+
+		shutdownError <- srv.Shutdown(ctx)
+	}()
+
+	err = srv.ListenAndServe()
+	if err != nil && err != http.ErrServerClosed {
+		log.Fatalf("Server failed: %v", err)
+	}
+
+	if err := <-shutdownError; err != nil {
+		log.Fatalf("Server shutdown error: %v", err)
+	}
 }
